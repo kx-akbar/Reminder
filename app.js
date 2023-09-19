f0218dd6a88411e5b9a06a8613e3c05308d8b237
Hojiakbar <xojiakbarisroilov777@gmail.com>
2023-09-19 17:41:45 +0500

    Reminder JS

--- app.js
+++ app.js
@@ -2,11 +2,17 @@
 
 const closeNavbar = document.getElementById('close');
 const leftNavbar = document.getElementById('navbar');
+const menuBar = document.getElementById('menuBar');
+const note = document.getElementById('note');
 
-
-
-
+menuBar.addEventListener('click', () => {
+    leftNavbar.style.display = "inline-block";
+    menuBar.removeAttribute('class', 'fa-solid fa-bars');
+    note.style.width = "70vw";
+})
 
 closeNavbar.addEventListener('click', () => {
     leftNavbar.style.display = "none";
+    note.style.width = "94.3vw";
+    menuBar.setAttribute('class', 'fa-solid fa-bars');
 });
