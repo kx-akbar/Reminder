30d966e5033373cd27172791665596414b74a730
Hojiakbar <xojiakbarisroilov777@gmail.com>
2023-09-19 17:09:09 +0500

    Reminder JS

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 "use strict";
 
-const changeBtn = document.getElementById('edit-btn');
-const imgModal = document.getElementById('modal');
-const closeModal = document.getElementById('close-modal');
-const dd = document.getElementById('img-modal');
-const nameInp = document.getElementById('name-inp');
-const userName = document.getElementById('user-name');
-const changeNameBtn = document.getElementById('change-name');
-const form = document.querySelector('form');
+const closeNavbar = document.getElementById('close');
+const leftNavbar = document.getElementById('navbar');
+
+
+
+
+
+closeNavbar.addEventListener('click', () => {
+    leftNavbar.style.display = "none";
+});
